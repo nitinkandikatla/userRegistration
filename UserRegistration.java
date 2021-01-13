9cdd786a07e2a52c649058bb0698c3ccd95ab2fa
nitinkandikatla <nitinkandikatla99@gmail.com>
2021-01-13 19:56:14 +0530

    ADDED : checking validity of last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -10,9 +10,19 @@ public class UserRegistration {
 		String regex ="^[A-Z]{1}[a-z]{2,}";
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter First Name :");
-		String s=sc.nextLine();
+		String s1=sc.nextLine();
+		System.out.println("Enter Last Name :");
+		String s2=sc.nextLine();
 		Pattern p=Pattern.compile(regex);
-		Matcher matcher=p.matcher(s);
+		System.out.println("First Name :");
+		pattern(s1,p);
+		System.out.println("Last Name :");
+		pattern(s2,p);
+	}
+	public static void pattern(String str,Pattern p1)
+	{
+		
+		Matcher matcher=p1.matcher(str);
 		if (matcher.matches()) 
 		{
 			System.out.println("valid");
