7db940dd8f408f0663692c02403e336ad53413a7
nitinkandikatla <nitinkandikatla99@gmail.com>
2021-01-13 20:10:57 +0530

    ADDED : checking validity of email

--- UserRegistration.java
+++ UserRegistration.java
@@ -8,21 +8,27 @@ public class UserRegistration {
 	public static void main(String[] args) {
 			
 		String regex ="^[A-Z]{1}[a-z]{2,}";
+		String email="^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter First Name :");
 		String s1=sc.nextLine();
 		System.out.println("Enter Last Name :");
 		String s2=sc.nextLine();
+		System.out.println("Enter Email :");
+		String s3=sc.nextLine();
 		Pattern p=Pattern.compile(regex);
+		Pattern p1=Pattern.compile(email);
 		System.out.println("First Name :");
 		pattern(s1,p);
 		System.out.println("Last Name :");
 		pattern(s2,p);
+		System.out.println("Email :");
+		pattern(s3,p1);
 	}
-	public static void pattern(String str,Pattern p1)
+	public static void pattern(String str,Pattern pat)
 	{
 		
-		Matcher matcher=p1.matcher(str);
+		Matcher matcher=pat.matcher(str);
 		if (matcher.matches()) 
 		{
 			System.out.println("valid");
